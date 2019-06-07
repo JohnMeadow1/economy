210c8e65d8e5a6110785b1f108d7a6bc961b2927
LAN-Komputer\LAN <leszek.nowak@uj.edu.pl>
2019-06-08 01:30:51 +0200

    added better resource cycle handling

--- Scripts/Resource.gd
+++ Scripts/Resource.gd
@@ -8,14 +8,15 @@ export(ResourceType) var _resource_type:int = 0 setget _set_resource_type
 
 func _ready():
 	randomize()
-	generate()
+#	generate()
 	update_display()
+	harvest_cost = max(1,harvest_cost)
 
 func _set_resource_name(value):
 	resource_name = value
 	if has_node("name"):
 		$name.text = value
-	
+
 func _get_resource_name():
 	return resource_name
 
@@ -29,29 +30,37 @@ func _set_resource_type(value):
 		self.resource_name = "null resource"
 		if has_node("Sprite"):
 			$Sprite.texture = load("res://icon.png")
-			
-			
+
 
 func _physics_process(delta):
-	cycle += delta
-	if cycle > regeneration_cycle:
-		cycle -= regeneration_cycle
-		resolve_cycle()
-
-func resolve_cycle():
+	if !Engine.is_editor_hint(): # do not calculate in editor
+		cycle += delta
+		if cycle > regeneration_cycle:
+			cycle -= regeneration_cycle
+			harvest()
+			availible_fluctuations -= availible
+			update_display()
+			
+func harvest():
+	availible_fluctuations = availible
 	availible += regenerates_per_cycle
-	harvested += auto_harvest
-	
+	var hervested = workers / harvest_cost + auto_harvest
+	hervested = min( hervested, harvestable_per_cycle)
+	hervested = min( hervested, availible)
+	stockpile += hervested
+	availible -= hervested
+	availible_fluctuations -= availible
+
 func update_display():
-	$values.text = str(floor(availible))+"\n"
+	$values.text = str(floor(availible))+ " ("
+	if availible_fluctuations <0: $values.text += "+"+str(-availible_fluctuations)+"/s)\n"
+	else: $values.text += str(-availible_fluctuations)+"/s)\n"
 	$values.text += str(harvest_cost)+"\n"
 	$values.text += str(regenerates_per_cycle)+"\n"
-	$values.text += str(worker_capacity)+"\n"
-	
+	$values.text += str(workers)+"/"+str(worker_capacity)
+
 func generate():
 	availible = 50 * (randi() % 7 + 1)              # randi between 50 and 350 with 50 step
 	regenerates_per_cycle = ceil(4 * randf()) + 1   # randf [1,5]
 	worker_capacity = randi() % 10 + 11             # randi [11,20]
-	
-func harvest():
-	pass
+
--- Scripts/base_classes/GameResource.gd
+++ Scripts/base_classes/GameResource.gd
@@ -6,7 +6,7 @@ const RESOURCE_WOOD = preload("res://Sprites/Farm/Tree_2_Side.png")
 const RESOURCE_STONE = preload("res://Sprites/Rocks/Gray_Rock.png")
 enum ResourceType{ NO_RESOURCE = -1, RESOURCE_GRAIN, RESOURCE_WOOD, RESOURCE_STONE }
 var ResourceName = ["Food", "Wood", "Stone"]
-var ResourceSprites = ["res://Sprites/Plants/wheet.png",
+var ResourceSprites = ["res://Sprites/Plants/Wheat.png",
                        "res://Sprites/Farm/Tree_2_Side.png",
                        "res://Sprites/Rocks/Gray_Rock.png"]
 
@@ -15,19 +15,12 @@ export(float) var harvestable_per_cycle:float = 1.0
 export(float) var harvest_cost:float          = 1.0
 export(float) var auto_harvest:float          = 0.0
 export(float) var regenerates_per_cycle:float = 0.0
-export(float) var regeneration_cycle:float    = 0.0
-export(int) var worker_capacity:int           = 1.0
+export(float) var regeneration_cycle:float    = 1.0
+
+export(int) var worker_capacity:int           = 1
+
 
 var cycle:float     = 0.0 
-var harvested:float = 0.0
-
-func get_harvested(location, collectors):
-#	if !is_connected("harvested", location, "end_harvest"):
-#		connect("harvested", location, "end_harvest")
-#
-#	var amount = min(collectors/gatherCost, currAmount)
-#	emit_signal("harvested", amount) 
-#	print(amount, " food collected.")
-#	currAmount = max(0, currAmount - collectors/gatherCost)
-#	update_display()
-	pass
+var workers:int     = 1
+var stockpile:float = 0.0
+var availible_fluctuations: float = 0.0
