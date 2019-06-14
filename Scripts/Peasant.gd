d1fee02f231731b118d74f9cdcd05f0eec18304d
concernedsyndicate <futurewasthen@protonmail.ch>
2019-06-14 14:04:39 +0200

    peasant return (bugged)

--- Scripts/Peasant.gd
+++ Scripts/Peasant.gd
@@ -3,9 +3,9 @@ class_name Peasant
 
 
 var velocity = Vector2()
-var speed = 200
 var destination = Vector2()
 
+var returning = false
 
 func _ready():
 	$Sprite/AnimationPlayer.play("run")
@@ -13,7 +13,11 @@ func _ready():
 func _physics_process(delta):
 	position += destination * delta #for "sening peasants" in one cycle they need to have velocity ~ distance_to_run
 	if position.distance_to(destination) < 5:
-		queue_free()
+		if !returning:
+			destination = -destination
+			returning = true
+		else:
+			queue_free()
 
 #func move_towards(pos: Vector2):
 #	destination = pos
