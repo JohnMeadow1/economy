2c4efe1fba182017be8466ba077e39068781c12c
concernedsyndicate <futurewasthen@protonmail.ch>
2019-06-18 14:58:41 +0200

    peasant running speed
    now depends on cycle duration

--- Scripts/Peasant.gd
+++ Scripts/Peasant.gd
@@ -2,16 +2,20 @@ extends Node2D
 class_name Peasant
 
 
-var velocity = Vector2()
+#var velocity = Vector2()
 var destination = Vector2()
+var speed_factor: float
 
 var returning = false
 
 func _ready():
 	$Sprite/AnimationPlayer.play("run")
+	speed_factor = 2 / get_node("/root/Main").CYCLE_DURATION
 
 func _physics_process(delta):
-	position += destination * delta #for "sending peasants" in one cycle they need to have velocity ~ distance_to_run
+	# for "sending peasants" in one cycle they need to have velocity ~ distance_to_run
+	# we want peasant to go 2 * 'destination' distance in one CYCLE_DURATION
+	position += speed_factor * destination * delta
 	if !returning:
 		if position.distance_to(destination) < 5:
 			destination = -destination
@@ -20,7 +24,3 @@ func _physics_process(delta):
 	else:
 		if position.distance_to(Vector2.ZERO) < 5:
 			queue_free()
-
-#func move_towards(pos: Vector2):
-#	destination = pos
-#	velocity = destination.normalized() * speed
